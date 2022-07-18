00e6cacafab4ac1e17001f702b4df4b4772de06a
kmolik <48009091+kmolik@users.noreply.github.com>
2022-07-18 15:33:16 +0200

    added validation for app and improved routes

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,16 +1,41 @@
+const { validationResult } = require('express-validator');
 const Product = require("../models/product");
+
 
 exports.getAddProduct = (req, res, next) => {
     res.render('admin/edit-product', {
         pageTitle: 'Add Product',
         path: '/admin/add-product',
-        editing: false
+        editing: false,
+        hasError: false,
+        errorMessage: null,
+        validationErrors: []
     });
 };
 
 exports.postAddProduct = (req, res, next) => {
     const { title, imageUrl, price, description } = req.body;
     const product = new Product({title: title, imageUrl: imageUrl, price: price, description: description, userId: req.user});
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(422).render('admin/edit-product', {
+            pageTitle: 'Add Product',
+            path: '/admin/edit-product',
+            editing: false,
+            hasError: true,
+            product: {
+                title: title,
+                imageUrl: imageUrl,
+                price: price,
+                description: description
+            },
+            errorMessage: errors.array()[0].msg,
+            validationErrors: errors.array()
+
+        });
+    }
+
     product
         .save()
         .then(() => {
@@ -22,7 +47,7 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
     Product
-        .find()
+        .find({userId: req.user._id})
         .then(products => {
             res.render('admin/products', {
                 prods: products,
@@ -50,7 +75,10 @@ exports.getEditProduct = (req, res, next) => {
                 pageTitle: 'Edit Product',
                 path: '/admin/edit-product',
                 editing: editMode,
-                product: product
+                product: product,
+                hasError: false,
+                errorMessage: null,
+                validationErrors: []
             });
         })
         .catch(err => console.log(err));
@@ -62,19 +90,43 @@ exports.postEditProduct = (req, res, next) => {
     const updatedPrice = req.body.price;
     const updatedImageUrl = req.body.imageUrl;
     const updatedDesc = req.body.description;
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(422).render('admin/edit-product', {
+            pageTitle: 'Edit Product',
+            path: '/admin/edit-product',
+            editing: true,
+            hasError: true,
+            product: {
+                title: updatedTitle,
+                imageUrl: updatedImageUrl,
+                price: updatedPrice,
+                description: updatedDesc,
+                _id: prodId
+            },
+            errorMessage: errors.array()[0].msg,
+            validationErrors: errors.array()
+
+        });
+    }
 
     Product
         .findById(prodId)
         .then(product => {
+            if (product.userId.toString() !== req.user._id.toString()) {
+                return res.redirect('/');
+            }
             product.title = updatedTitle;
             product.price = updatedPrice;
             product.imageUrl = updatedImageUrl;
             product.description = updatedDesc;
-            return product.save();
-        })
-        .then(() => {
-            console.log('Product updated successfully');
-            res.redirect('/admin/products');
+            return product
+                .save()
+                .then(() => {
+                console.log('Product updated successfully');
+                res.redirect('/admin/products');
+            });
         })
         .catch(err => console.log(err));
 }
@@ -83,7 +135,7 @@ exports.postDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
 
     Product
-        .findByIdAndRemove(prodId)
+        .deleteOne({_id: prodId, userId: req.user._id})
         .then(() => {
             console.log('Product deleted successfully');
             res.redirect('/admin/products');
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,8 @@
+const crypto = require('crypto');
+
 const bcrypt = require('bcryptjs');
 const sgMail = require('@sendgrid/mail');
+const { validationResult } = require('express-validator');
 
 sgMail.setApiKey('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');
 
@@ -15,7 +18,12 @@ exports.getLogin = (req, res, next) => {
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
-        errorMessage: message
+        errorMessage: message,
+        oldInput: {
+            email: '',
+            password: ''
+        },
+        validationErrors: []
     });
 };
 
@@ -29,18 +37,47 @@ exports.getSignup = (req, res, next) => {
     res.render('auth/signup', {
         path: '/signup',
         pageTitle: 'Signup',
-        errorMessage: message
+        errorMessage: message,
+        oldInput: {
+            email: '',
+            password: '',
+            confirmPassword: ''
+        },
+        validationErrors: []
     });
 };
 
 exports.postLogin = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).render('auth/login', {
+            path: '/login',
+            pageTitle: 'Login',
+            errorMessage: errors.array()[0].msg,
+            oldInput: {
+                email: email,
+                password: password
+            },
+            validationErrors: errors.array()
+        });
+    }
+
     User.findOne({ email: email })
         .then(user => {
             if (!user) {
-                req.flash('error', 'Invalid email or password.');
-                return res.redirect('/login');
+                return res.status(422).render('auth/login', {
+                    path: '/login',
+                    pageTitle: 'Login',
+                    errorMessage: 'Invalid email or password.',
+                    oldInput: {
+                        email: email,
+                        password: password
+                    },
+                    validationErrors: []
+                });
             }
             bcrypt
                 .compare(password, user.password)
@@ -53,11 +90,19 @@ exports.postLogin = (req, res, next) => {
                             res.redirect('/');
                         });
                     }
-                    req.flash('error', 'Invalid email or password.');
-                    res.redirect('/login');
+                    return res.status(422).render('auth/login', {
+                        path: '/login',
+                        pageTitle: 'Login',
+                        errorMessage: 'Invalid email or password.',
+                        oldInput: {
+                            email: email,
+                            password: password
+                        },
+                        validationErrors: []
+                    });
                 })
                 .catch(err => {
-                    console.log(err)
+                    console.log(err);
                     res.redirect('/login');
                 });
         })
@@ -67,41 +112,46 @@ exports.postLogin = (req, res, next) => {
 exports.postSignup = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
-    const confirmPassword = req.body.confirmPassword;
-    User
-        .findOne({ email: email })
-        .then(userDoc => {
-            if (userDoc) {
-                req.flash('error', 'Email exists already, please choose a different one.');
-                return res.redirect('/signup');
-            }
-            return bcrypt
-                .hash(password, 12)
-                .then(hashedPassword => {
-                    const user = new User({
-                        email: email,
-                        password: hashedPassword,
-                        cart: { items: [] }
-                    });
-                    return user.save();
-                })
-                .then(result => {
-                    const msg = {
-                        to: email,
-                        from: 'konrad.phoenix@gmail.com',
-                        subject: 'Signup succeeded!',
-                        html: '<h1>You successfully signed up!</h1>'
-                    };
-                    res.redirect('/login');
-                    return sgMail
-                        .send(msg)
-                        .catch(err => {
-                            console.log(err);
-                        })
-                });
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(422).render('auth/signup', {
+            path: '/signup',
+            pageTitle: 'Signup',
+            errorMessage: errors.array()[0].msg,
+            oldInput: {
+                email: email,
+                password: password,
+                confirmPassword: req.body.confirmPassword
+            },
+            validationErrors: errors.array()
+        });
+    }
+    bcrypt
+        .hash(password, 12)
+        .then(hashedPassword => {
+            const user = new User({
+                email: email,
+                password: hashedPassword,
+                cart: { items: [] }
+            });
+            return user.save();
         })
-        .catch(err => console.log(err));
-};
+        .then(result => {
+            const msg = {
+                to: email,
+                from: 'konrad.phoenix@gmail.com',
+                subject: 'Signup succeeded!',
+                html: '<h1>You successfully signed up!</h1>'
+            };
+            res.redirect('/login');
+            return sgMail
+                .send(msg)
+                .catch(err => {
+                    console.log(err);
+                })
+        });
+}
 
 exports.postLogout = (req, res, next) => {
     req.session.destroy(err => {
@@ -109,3 +159,106 @@ exports.postLogout = (req, res, next) => {
         res.redirect('/');
     });
 };
+
+exports.getReset = (req, res, next) => {
+    let message = req.flash('error');
+    if (message.length > 0) {
+        message = message[0];
+    } else {
+        message = null;
+    }
+    res.render('auth/reset', {
+        path: '/reset',
+        pageTitle: 'Reset Password',
+        errorMessage: message
+    });
+}
+
+exports.postReset = (req, res, next) => {
+    const email = req.body.email;
+    crypto.randomBytes(32, (err, buffer) => {
+        if (err) {
+            console.log(err);
+            return res.redirect('/reset');
+        }
+        const token = buffer.toString('hex');
+        User.findOne({email: email})
+            .then(user => {
+                if (!user) {
+                    req.flash('error', 'No account with that email found.');
+                    return res.redirect('/reset');
+                }
+                user.resetToken = token;
+                user.resetTokenExpiration = Date.now() + 3600000;
+                return user.save();
+            })
+            .then(result => {
+                const msg = {
+                    to: email,
+                    from: 'konrad.phoenix@gmail.com',
+                    subject: 'Password Reset',
+                    html: `
+                        <p>You requested a password reset</p>
+                        <p>Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password.</p>
+                    `
+                }
+                return sgMail
+                    .send(msg)
+                    .catch(err => {
+                        console.log(err);
+                    })
+            })
+            .then(result => {
+                req.flash('success', 'Success! Check your email for the link to reset your password.');
+                res.redirect('/login');
+            })
+            .catch(err => console.log(err));
+    });
+}
+
+exports.getNewPassword = (req, res, next) => {
+    const token = req.params.token;
+    User.
+        findOne({
+            resetToken: token,
+            resetTokenExpiration: { $gt: Date.now() }
+        })
+        .then(user => {
+            let message = req.flash('error');
+            if (message.length > 0) {
+                message = message[0];
+            } else {
+                message = null;
+            }
+            res.render('auth/new-password', {
+                path: '/new-password',
+                pageTitle: 'New Password',
+                errorMessage: message,
+                userId: user._id.toString(),
+                passwordToken: token
+            });
+        }).catch(err => console.log(err));
+}
+
+exports.postNewPassword = (req, res, next) => {
+    const newPassword = req.body.password;
+    const userId = req.body.userId;
+    const passwordToken = req.body.passwordToken;
+    let resetUser;
+    User.findOne({
+        resetToken: passwordToken,
+        resetTokenExpiration: { $gt: Date.now() },
+        _id: userId
+    }).then(user => {
+        resetUser = user;
+        return bcrypt.hash(newPassword, 12);
+    }).then(hashedPassword => {
+        resetUser.password = hashedPassword;
+        resetUser.resetToken = undefined;
+        resetUser.resetTokenExpiration = undefined;
+        return resetUser.save();
+    }).then(result => {
+        req.flash('success', 'Success! Your password has been changed.');
+        res.redirect('/login');
+    }).catch(err => console.log(err));
+}
